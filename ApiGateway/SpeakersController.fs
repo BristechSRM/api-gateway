39fb385d0eae33b8b813ff0be8fd36834474b6fd
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-16 14:22:05 +0100

    Add put method and backing functions

--- ApiGateway/SpeakersController.fs
+++ ApiGateway/SpeakersController.fs
@@ -1,5 +1,6 @@
 ﻿namespace Controllers
 
+open System.Net
 open System.Net.Http
 open System.Web.Http
 open Serilog
@@ -15,3 +16,13 @@ type SpeakersController() =
         match getSpeaker id with
         | Success speaker -> x.Request.CreateResponse(speaker)
         | Failure error -> x.Request.CreateResponse(error.HttpStatusCode, error.Body)
+
+    member x.Put(id : Guid, updatedSpeaker : Speaker) = 
+        Log.Information("Received Put request for speaker with id {id}", id)
+        match updateSpeaker id updatedSpeaker with
+        | Success speaker -> 
+            Log.Information("Put succeeded")
+            x.Request.CreateResponse(speaker)
+        | Failure error -> 
+            Log.Error("Put failed with StatusCode: {code} and message {body}",error.HttpStatusCode, error.Body)
+            x.Request.CreateErrorResponse(error.HttpStatusCode, error.Body)
