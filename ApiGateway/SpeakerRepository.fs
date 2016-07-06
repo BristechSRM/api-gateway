a1d81370173485dc6c9ef4c9e87127210e35727d
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-06 16:46:34 +0100

    Add get*Summary functions

--- ApiGateway/SpeakerRepository.fs
+++ ApiGateway/SpeakerRepository.fs
@@ -4,7 +4,9 @@ open HandlesRepository
 open ProfilesRepository
 open DataTransform
 
-let getSpeaker speakerId= 
+let getSpeaker speakerId = 
     let profile = getProfile speakerId 
     let handlesDtos = getHandlesByProfileId speakerId 
     Profile.toSpeaker handlesDtos profile
+
+let getSpeakerSummary speakerId = getProfile speakerId |> Profile.toSpeakerSummary
