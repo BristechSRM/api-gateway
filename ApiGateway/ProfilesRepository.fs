8f9525af318b3e0a319ceb058869fa4828b0929c
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-06 13:49:20 +0100

    Remove updateProfile and putSpeaker

--- ApiGateway/ProfilesRepository.fs
+++ ApiGateway/ProfilesRepository.fs
@@ -2,23 +2,11 @@
 
 open Config
 open System
-open System.Text
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
 open Serilog
 open Dtos
-open Models
-open DataTransform
-
-let speakerToProfile (speaker : Speaker) : Profile = 
-    { Id = speaker.Id 
-      Forename = speaker.Forename
-      Surname = speaker.Surname
-      Rating = speaker.Rating
-      ImageUrl = speaker.ImageUri
-      Bio = speaker.Bio }
-      //Handles = speaker.Handles |> Seq.map handleToHandleDto }
 
 let getProfile(pid : Guid) =
     use client = new HttpClient()
@@ -34,32 +22,3 @@ let getProfile(pid : Guid) =
         let message = sprintf "Error Fetching profile: Status code: %A. Reason: %s" result.StatusCode result.ReasonPhrase
         Log.Error(message)
         raise <| Exception(message)
-
-//Currently broken. Handles aren't dealt with since sessions change. 
-let updateProfile (pid : Guid) (profile : Profile) = 
-    use client = new HttpClient()
-
-    try
-        let data = JsonConvert.SerializeObject(profile)
-        let content = new StringContent(data,Encoding.UTF8,"application/json")
-        let result = client.PutAsync(profilesUrl + pid.ToString(),content).Result
-
-        match result.StatusCode with
-        | HttpStatusCode.OK -> Success <| getProfile pid
-        | _ -> 
-            Log.Information("Error updating profile Status code: {statusCode}. Reason: {reasonPhrase}", result.StatusCode, result.ReasonPhrase)
-            Failure { HttpStatusCode = result.StatusCode; Body = result.ReasonPhrase }
-    with
-        | ex ->
-            Log.Error("Unhandled exception: {message}", ex)
-            Failure { HttpStatusCode = HttpStatusCode.InternalServerError; Body = "An unhandled error occurred:\n" + ex.ToString() }
-
-let updateSpeaker sid (speaker : Speaker) = 
-    if sid <> speaker.Id then
-        Failure { HttpStatusCode = HttpStatusCode.BadRequest 
-                  Body = "Invalid Data. specified speaker Id in request url does not match Id of input speaker"}
-    else 
-        let profile = speakerToProfile speaker
-        match updateProfile sid profile with
-        | Success profile -> Profile.toSpeaker [] profile |> Success //TODO handles
-        | Failure error -> Failure error
--- ApiGateway/SpeakersController.fs
+++ ApiGateway/SpeakersController.fs
@@ -3,11 +3,9 @@
 open System.Net
 open System.Net.Http
 open System.Web.Http
-open Serilog
 open HandlesRepository
 open ProfilesRepository
 open System
-open Models
 open DataTransform
 
 type SpeakersController() =
@@ -19,13 +17,3 @@ type SpeakersController() =
             let handlesDtos = getHandlesByProfileId id 
             Profile.toSpeaker handlesDtos profile) 
         |> Catch.respond x HttpStatusCode.OK 
-
-    member x.Put(id : Guid, updatedSpeaker : Speaker) = 
-        Log.Information("Received Put request for speaker with id: {id}", id)
-        match updateSpeaker id updatedSpeaker with
-        | Success speaker -> 
-            Log.Information("Success: Put request for speaker with id: {id} succeeded", id)
-            x.Request.CreateResponse(speaker)
-        | Failure error -> 
-            Log.Error("Put failed with StatusCode: {code} and message {body}",error.HttpStatusCode, error.Body)
-            x.Request.CreateErrorResponse(error.HttpStatusCode, error.Body)
