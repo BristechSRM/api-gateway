13699c432f2a019e672f8381b1be406efb27796f
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-08-30 10:37:31 +0100

    Remove unused imports

--- Gateway/SessionIdsProxy.fs
+++ Gateway/SessionIdsProxy.fs
@@ -1,9 +1,7 @@
 ﻿module SessionIdsProxy
 
 open Config
-open Dtos
 open JsonHttpClient
-open RestModels
 open System
 
 let getSessionIdsByEventId (eventId : Guid) = get<Guid []> <| new Uri(sessionIdsUri, "?eventId=" + eventId.ToString())
