a75082e69cc8c53fa682d31f3580f39aa831604f
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-08-30 11:11:57 +0100

    Formatting

--- Gateway/EventsProxy.fs
+++ Gateway/EventsProxy.fs
@@ -5,11 +5,8 @@ open Dtos
 open JsonHttpClient
 open System
 
-let getEvents() =
-  get<Event []> eventsUri
+let getEvents() = get<Event []> eventsUri
 
-let getEvent(id : Guid) =
-  get<Event> <| new Uri(eventsUri, id.ToString())
+let getEvent(id : Guid) = get<Event> <| new Uri(eventsUri, id.ToString())
 
-let postEvent(event: Event) =
-  postAndGetGuid eventsUri event
+let postEvent(event: Event) = postAndGetGuid eventsUri event
