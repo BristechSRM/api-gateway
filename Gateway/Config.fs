afebe85e45d7c96b35ecdead4a9f0e9e944cb1b7
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-01 12:47:40 +0100

    Correct uri slashes

--- Gateway/Config.fs
+++ Gateway/Config.fs
@@ -15,10 +15,12 @@ let baseUrl = getConfigValue "BaseUrl"
 let sessionsServiceUri = getUriConfigValue "SessionsServiceUrl"
 let commsServiceUri = getUriConfigValue "CommsServiceUrl"
 
-let handlesUri = Uri (sessionsServiceUri, "handles")
-let sessionsUri = Uri (sessionsServiceUri, "sessions")
-let sessionIdsUri = Uri (sessionsServiceUri, "sessions/ids")
-let profilesUri = Uri (sessionsServiceUri, "profiles")
-let eventsUri = Uri (sessionsServiceUri, "events")
-let lastContactUri = Uri (commsServiceUri, "last-contact")
-let correspondenceUri = Uri (commsServiceUri, "correspondence")
+//Note : when using the Uri constructor, slash must be on the end of a relative url that will have other parts attached
+//Alternatively, use UriBuilder if things get more complicated. 
+let handlesUri = Uri (sessionsServiceUri, "handles/")
+let sessionsUri = Uri (sessionsServiceUri, "sessions/")
+let sessionIdsUri = Uri (sessionsServiceUri, "sessions/ids/")
+let profilesUri = Uri (sessionsServiceUri, "profiles/")
+let eventsUri = Uri (sessionsServiceUri, "events/")
+let lastContactUri = Uri (commsServiceUri, "last-contact/")
+let correspondenceUri = Uri (commsServiceUri, "correspondence/")
