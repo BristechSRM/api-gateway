6153f6f14ed1ad002f71eedf1d5e724b7bfac62b
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-10-04 09:36:51 +0100

    Add notes Patch

--- Gateway/NotesController.fs
+++ Gateway/NotesController.fs
@@ -5,6 +5,7 @@ open System.Net
 open System.Web.Http
 open NotesProxy
 open DataTransform
+open RestModels
 
 type NotesController() = 
     inherit ApiController()
@@ -13,3 +14,5 @@ type NotesController() =
     member x.GetBySessionId(sessionId : Guid) = (fun () -> getNotesBySessionId sessionId |> Seq.map Note.toModel) |> Catch.respond x HttpStatusCode.OK
 
     member x.Post(note : Models.Note) = (fun () -> note |> Note.toDto |> postNote) |> Catch.respond x HttpStatusCode.Created
+
+    member x.Patch(id : Guid, op : PatchOp) = (fun () -> patchNote id op) |> Catch.respond x HttpStatusCode.NoContent
